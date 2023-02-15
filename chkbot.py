85009e97149078bd24edf3dff575490206c000e6
ajajw <111954733+ajajw@users.noreply.github.com>
2023-02-15 16:49:06 -0500

    Update chkbot.py

--- chkbot.py
+++ chkbot.py
@@ -253,7 +253,8 @@ async def pene(message: types.Message):
 
     api201 = session.post("https://api.stripe.com/v1/payment_intents/pi_3MbaLOJeGhFfMJgC1jwsOBWM/confirm", headers=headels, data=dat4).json()
     
-    await message.reply(f"{api201}")
+    ko = api201["error"]["code"]
+    await message.reply(f"{ko}")
 
 print("CODIGO EN LINEA")
 logging.basicConfig(level=logging.INFO)
